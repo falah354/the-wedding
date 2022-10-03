03d037e16413268fbad1602097862d4226dc2a2d
falah354 <114891828+falah354@users.noreply.github.com>
2022-10-03 09:38:27 +0700

    Create lokasi.php
    lokasi

---
+++ lokasi.php
@@ -0,0 +1,14 @@
+<html>
+    <style>
+body{
+    <style>
+font-family: sans-serif;
+	background: #FFE4C4;
+}
+    </style>
+<title>LOKASI ACARA</title>
+<h1 align="center"><img src="lokasi.jpg" style="width:500px;height:400;"/></h1><br><br>
+<h2 style="background-color: #f8f8f7; color: rgb(255, 208, 0); text-align: center; font-family: 'Baby Aletha', Baby Aletha, Baby Aletha;" class="heading"><a href="https://maps.app.goo.gl/kWAzjAdR5GBGqZ17A">LOKASI</a>
+<br><br>
+<align="center"><a href="undangan3.html"> HOME </a></h2>
+</html>
