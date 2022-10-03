165495aa0ef51a41930d245e01a4328ed0992b8c
falah354 <114891828+falah354@users.noreply.github.com>
2022-10-03 09:37:29 +0700

    Create isi_undangan.php
    isi undangan

---
+++ isi_undangan.php
@@ -0,0 +1,286 @@
+<html>
+<head>
+    
+ <style>
+		body{
+	font-family: sans-serif;
+	background: #FFE4C4;
+}
+		
+
+</style>
+<style>
+figure, figcaption {
+display: block;
+}
+#polaroid{
+width:100%;
+padding:0px 10px;
+margin:auto;
+text-align: center;
+}
+#polaroid figure{
+position:relative;
+width: auto;
+max-width: 400px; /* Lebar maksimal gambar */
+margin: 20px auto 0px;
+padding: 6px 8px 10px 8px;
+display:inline-block;
+-webkit-box-shadow: 4px 4px 8px -4px rgba(0, 0, 0, .75);
+-moz-box-shadow: 4px 4px 8px -4px rgba(0, 0, 0, .75);
+box-shadow: 4px 4px 8px -4px rgba(0, 0, 0, .75);
+background-color: #eee6d8;
+-webkit-transform:rotate(-1deg);
+-moz-transform: rotate(-1deg);
+-o-transform: rotate(-1deg);
+-ms-transform: rotate(-1deg);
+transform: rotate(-1deg);
+-webkit-backface-visibility:hidden;
+}
+#polaroid figure:nth-child(even) {
+margin:20px 18px 20px 25px;
+-webkit-transform:rotate(2deg);
+-moz-transform: rotate(2deg);
+-o-transform: rotate(2deg);
+-ms-transform: rotate(2deg);
+transform: rotate(2deg);
+-webkit-backface-visibility:hidden;
+-webkit-box-shadow: 4px 4px 8px -4px rgba(0, 0, 0, .75);
+-moz-box-shadow: 4px 4px 8px -4px rgba(0, 0, 0, .75);
+box-shadow: -4px 4px 8px -4px rgba(0, 0, 0, .75);
+}
+#polaroid figure:before {
+content: '';
+display: block;
+position: absolute;
+left: 5px;
+top: -10px;
+width: 95px;
+height: 35px;
+z-index: 1;
+background-color: rgba(222,220,198,0.7);
+-webkit-transform: rotate(-12deg);
+-moz-transform: rotate(-12deg);
+-o-transform: rotate(-12deg);
+-ms-transform: rotate(-12deg);
+}
+#polaroid figure:nth-child(even):before {
+left:unset;
+right:10px;
+top:-10px;
+width: 55px;
+height: 25px;
+z-index: 1;
+-webkit-transform: rotate(12deg);
+-moz-transform: rotate(12deg);
+-o-transform: rotate(12deg);
+-ms-transform: rotate(12deg);
+}
+#polaroid img{
+max-width: 100%;
+width: 100%;
+height: auto;
+}
+#polaroid figcaption{
+text-align:center;
+font-family: cursive, Arial, Helvetica, sans-serif;
+color:#454f40;
+font-style:italic;
+letter-spacing:0.09em;
+margin-top:10px;
+}
+
+
+</style>
+
+
+
+
+<audio autoplay>
+    <source src="y2mate.com - Payung Teduh  Akad Lirik.mp3" type="audio/mp3">
+    Browsermu tidak mendukung tag audio, upgrade donk!
+</audio>
+<div id="polaroid">
+  <figure>
+    <img src="foto gariel.jpeg" alt="Tulis Judul yang SEO Friendly" title="Tulis Judul yang SEO Friendly"/>
+    <figcaption><H3>THE WEDDING</H3></figcaption>
+  </figure>
+  
+<h1 style=" text-align: center; font-family: cursive, Arial, Helvetica, sans-serif;" class="center"> 
+You are invited to the wedding <hr /><br></h1>s
+<h2 style=" text-align: center; font-family: cursive, Arial, Helvetica, sans-serif;"> ASSALAMUALAIKUM WARAHMATULLAHI WABARAKATU<BR>
+Dengan Memohon Rahmat Dan Ridho Allah SWT, <br>
+Kami Bermaksud Menyelenggarakan Acara  Pernikahan Kami, <br><br>
+<body>
+<table align="center" width=700" height="217" border="0">
+<tr>
+ 
+      <td width="204" height="211"><div align="center"><img src="gabriel2.jpg" width="250" height="250" alt="GABRIEL" /></div></td>
+    <td width="150"><div align="center"> </div></td>
+
+      <td width="204"><div align="center"><img src="keisya2.jpg" width="250" height="250" alt="KEISYA" /></div></td>
+    </tr>
+  </table></td>
+</tr>
+</table>
+<h1 style=" text-align: center; font-family: cursive, Arial, Helvetica, sans-serif;" class="center"> GABRIEL PRINCE <br>
+<h4 style=" text-align: center; font-family: cursive, Arial, Helvetica, sans-serif;" class="center"> Putra dari bapak Soerijo Gondo Setiawan </h4>
+<h1 style=" text-align: center; font-family: cursive, Arial, Helvetica, sans-serif;" class="center">&<br></h1>
+<h1 style=" text-align: center; font-family: cursive, Arial, Helvetica, sans-serif;" class="center">KEISYA LEVRONKA </h1>
+<h4 style=" text-align: center; font-family: cursive, Arial, Helvetica, sans-serif;" class="center">Putri dari bapak Havron Liebchen </h4><hr /><br><br>
+ <h3 style=" text-align: center; font-family: cursive, Arial, Helvetica, sans-serif;" class="center">"kami akan menikah, dan kami ingin anda menjadi bagian dari hari istimewa kami" <BR><BR><BR></H3>
+<h1 align="center"> JADWAL ACARA </h1>
+
+<body>
+
+	<H3 align="center" class="ketengah">
+		<H3 class="kotak kotak1">AKAD NIKAH<br><br>
+        SABTU,12 JANUARI 2023<br>
+    JAM 07:30 - 09:10 WIB</H3>
+        
+        <style>
+        body{
+	background:  #FFE4C4;
+	font-family: roboto;
+	text-align: center;
+}
+ 
+h1{
+	color: #fff;
+}
+ 
+.ketengah{
+	margin: 10px auto;
+	width: 1150px;
+}
+ 
+.kotak{
+	background: 	 #FFE4C4;
+	padding: 30px;
+	width: 100px;
+	float: left;
+	margin: 30px;
+	height: 100px;
+}
+ 
+.kotak1{
+	border-radius: 10px 10px 10px 10px;
+}
+
+ </style>
+ <body>
+	
+	<H1 align="center" class="ketengah">
+		<H1 class="kotak kotak6">save the date</H1>
+		
+        <style>
+        body{
+	background:  #FFE4C4;
+	font-family: roboto;
+	text-align: center;
+}
+ 
+h1{
+	color: #fff;
+}
+ 
+.ketengah{
+	margin: 10px auto;
+	width: 1150px;
+}
+ 
+.kotak{
+	background: 	 #FFE4C4;
+	padding: 30px;
+	width: 100px;
+	float: left;
+	margin: 50px;
+	height: 100px;
+}
+ 
+.kotak6{
+	border-radius: 100%;
+}
+
+ </style>
+ <body>
+	
+	<H3 align="center" class="ketengah">
+		<H3 class="kotak kotak1">RESEPSI NIKAH<br><br>
+        SABTU,12 JANUARI 2023<br>
+    JAM 09:00 - 16:00 WIB</H3>
+        
+        <style>
+        body{
+	background:  #FFE4B5;
+	font-family: roboto;
+	text-align: center;
+}
+ 
+h1{
+	color: #8B0000;
+}
+ 
+.ketengah{
+	margin: 10px auto;
+	width: 1150px;
+}
+ 
+.kotak{
+	background: 	#F4A460;
+	padding: 30px;
+	width: 120px;
+	float: left;
+	margin: 30px;
+	height: 200px;
+}
+ 
+.kotak1{
+	border-radius: 10px 10px 10px 10px;
+}
+
+ </style>      
+ 
+ <img align="center" src="lokasi.JPG"><br><br>
+ <h2 style="background-color: #f8f8f7; color: rgb(255, 208, 0); text-align: center; font-family: 'Baby Aletha', Baby Aletha, Baby Aletha;" class="heading"><a href="https://maps.app.goo.gl/kWAzjAdR5GBGqZ17A">LOKASI</a><br><br>
+ <align="center"><a href="undangan3.html"> HOME </a></h2>
+<style>
+	
+}
+figure, figcaption {
+display: block;
+}
+#polaroid{
+width:100%;
+padding:0px 10px;
+margin:auto;
+text-align: center;
+}
+#polaroid figure:nth-child(even) {
+margin:20px 18px 20px 25px;
+-webkit-transform:rotate(2deg);
+-moz-transform: rotate(2deg);
+-o-transform: rotate(2deg);
+-ms-transform: rotate(2deg);
+transform: rotate(2deg);
+-webkit-backface-visibility:hidden;
+-webkit-box-shadow: 4px 4px 8px -4px rgba(0, 0, 0, .75);
+-moz-box-shadow: 4px 4px 8px -4px rgba(0, 0, 0, .75);
+box-shadow: -4px 4px 8px -4px rgba(0, 0, 0, .75);
+}
+}
+#polaroid img{
+max-width: 100%;
+width: 100%;
+height: auto;
+}
+	
+
+</style>
+<div id="polaroid">
+  <figure>
+    <img src="bunga2.png" alt="Tulis Judul yang SEO Friendly" title="Tulis Judul yang SEO Friendly"/>
+</figure>
+</div>
+</div>
+</html>
